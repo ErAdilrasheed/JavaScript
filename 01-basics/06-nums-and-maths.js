8d24d7f30167c3352fc9f640eff460a5209a0256
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-01-05 18:55:47 +0000

    number and maths

---
+++ 01-basics/06-nums-and-maths.js
@@ -0,0 +1,35 @@
+const score= 400;
+//console.log(score);
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const newNumber = 123.9876
+//console.log(newNumber.toPrecision(4));
+
+const lakhs = 1000000
+//console.log(lakhs.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+/*console.log(Math);
+console.log(Math.abs(-90));
+console.log(Math.round(49.51));
+console.log(Math.ceil(4.09));
+console.log(Math.floor(4.89));
+console.log(Math.min(9,8,0,6,3));
+console.log(Math.max(6,3,0,8,1));*/
+
+//console.log(Math.random());
+//console.log((Math.random() *10) +1);
+//console.log(Math.floor((Math.random() *10) +1));
+
+const min = 10
+const max = 30
+console.log(Math.floor(Math.random() * (max - min +1)) +min);
+
+
+
+
