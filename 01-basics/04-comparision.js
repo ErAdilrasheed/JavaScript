d0409a550689209c6086a1192d49365f63ede7af
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-01-05 18:55:10 +0000

    comparision

---
+++ 01-basics/04-comparision.js
@@ -0,0 +1,27 @@
+/*console.log(44>74)
+console.log(44<74)
+console.log(44=74)
+console.log(44>=74)
+console.log(44<=74)
+console.log(44==74)*/
+
+/*console.log("33" > 23);
+console.log("33" > "23");
+console.log("33" >= 23);
+console.log(33 <= "23");*/
+
+//console.log("2" ===2);
+
+console.log(null>0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(null==0);
+
+
+
+console.log(undefined>0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+console.log(undefined==0);
+
+
