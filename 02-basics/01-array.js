cad03fa1f94beb5c43fba074ee705d18d66eaa97
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-01-19 14:07:50 +0000

    array

---
+++ 02-basics/01-array.js
@@ -0,0 +1,35 @@
+//Array
+const myArr = [9,8,7,6,5];
+//console.log(myArr);
+
+const myHeroes = ["Hitesh", "Kunal", 7,8,6];
+//console.log(myHeroes);
+
+const myArr2 = new Array(9,5,8,4,0);
+//console.log(myArr[2]);
+
+//Array Method
+//myArr.push(8);
+//myArr.push(9);
+//myArr.pop();
+
+//myArr.unshift(6)
+//myArr.shift()
+
+//console.log(myArr.includes(4));
+//console.log(myArr.indexOf(4));
+//console.log(myArr);
+
+const newArr = myArr.join()
+//console.log(newArr);
+//console.log(myArr);
+
+// slice & splice
+
+console.log("A" , myArr);
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+
+console.log("B" , myArr);
+const myn2 = myArr.splice(1,3);
