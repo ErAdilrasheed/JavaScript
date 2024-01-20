a37af1eab42bf17cde463b663829b77bf96bbc66
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-01-20 10:36:17 +0000

    arrays

---
+++ 02-basics/02-array.js
@@ -0,0 +1,24 @@
+//const myFevCricketer = ["Sachin","Virat","Shami"];
+//const myFevFootballer = ["CR7", "Messi","Zidan"];
+//myFevCricketer.push(myFevFootballer)
+//console.log(myFevCricketer);
+
+//const anotherFevPlayer = myFevCricketer.concat(myFevFootballer);
+//console.log(anotherFevPlayer);
+
+//spred operator
+
+//const myAllplayer = [...myFevCricketer, ...myFevFootballer]
+//console.log(myAllplayer);
+
+//const anotherArray = [9,6,1,[6,3,1,], 6, [[9,2],[5]]];
+//const new_real_array = anotherArray.flat(Infinity);
+//console.log(new_real_array);
+//console.log(Array.isArray("Adil Rasheed"));
+//console.log(Array.from("Adil Rasheed"));
+//console.log(Array.from{name: "Adil Rasheed"});  //intersting
+
+let score1 = 272
+let score2 = 234
+let score3 = 290
+console.log(Array.of(score1, score2, score3));
