8bce5c7c60783185e32c25489ae9e48a7b2489fc
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-01-21 10:38:03 +0000

    objects

---
+++ 02-basics/04-object.js
@@ -0,0 +1,9 @@
+//const newUser = new Object()   // singleton object
+const newUser = {}  //Object Literals
+console.log(newUser);
+
+newUser.id = "123abc"
+newUser.name = "Mohit"
+newUser.isLoggedIn = true
+
+console.log(newUser);
