75a8a50f22a59ca719005e03a5167371c67b7074
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-02-04 15:39:03 +0000

    functions

--- 03-basics/01-functions.js
+++ 03-basics/01-functions.js
@@ -1,20 +1,20 @@
-function myName(){
+//function myName(){
     // console.log("A");
     // console.log("D");
     // console.log("I");
-    // console.log("L");
-}
-myName // function reference
-myName() // function execute
+//     // console.log("L");
+// }
+// myName // function reference
+// myName() // function execute
 
-function addTwoNumbers(Number1 , Number2){   // it's called function parameter
+//function addTwoNumbers(Number1 , Number2){   // it's called function parameter
     //console.log(Number1 + Number2);
-}
+//}
 // addTwoNumbers(44,74)     // function argument
 // addTwoNumbers(44,"abc")    // use checking methode like if -else before this type of operation
 // addTwoNumbers(44, null)
 
-const result = addTwoNumbers(10,20)
+//const result = addTwoNumbers(10,20)
 //console.log("Result :" , result);
 
 // function addTwoNumbers(number1 , number2){
@@ -24,17 +24,41 @@ const result = addTwoNumbers(10,20)
 
 // addTwoNumbers(55, 45)
 
-function userLoginMessage(username = "Default User"){
-    if(!username){
-        console.log("please enter a correct username");
-        return
-    }
-   return `${username} just loggedin`
-}
+// function userLoginMessage(username = "Default User"){
+//     if(!username){
+//         console.log("please enter a correct username");
+//         return
+//     }
+//    return `${username} just loggedin`
+// }
 //console.log(userLoginMessage("Adil Rasheed"));
 
 // console.log(userLoginMessage()); // if don't pass anything then result undefined
 
 //console.log(userLoginMessage("Adil Rasheed"));
 
-function cartCalculatePrice(num1)
+// function calculateCartPrice(val1, val2,...num1){  //... known as rest operator or sprade operator
+//     return num1
+// }
+//console.log(calculateCartPrice(200 ,400, 500 , 2000))  // val1 stores 200 and val2 stores 400 and rest are stores in ...num1
+const user = {
+ username: "Adil Rasheed" , 
+ price: "999"
+}
+
+function objectHandle(anyobject){
+//console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//objectHandle(user)
+objectHandle({
+    username: "Adil",
+    price: "700"
+})
+
+const myNewArray = [500, 600, 700, 800]
+function findLastValue(getArray){
+    return getArray[3]
+}
+//console.log(findLastValue(myNewArray));
+
+console.log(findLastValue([500, 600, 700, 800]));
