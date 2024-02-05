c236ace612edf5903c9707c78975876e7eddf3c5
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-02-05 17:46:56 +0000

    switch cases

---
+++ 04-control-flow/02-switch.js
@@ -0,0 +1,53 @@
+const month = 3
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+
+        case 2:
+        console.log("Feb");
+        break;
+
+        case 3:
+        //console.log("march");
+        break;
+
+        case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("No month selected");
+        break;
+}
+
+const Days = "Friday"
+switch (Days) {
+    case "Monday":
+        console.log("Mood Off");
+        break;
+
+        case "Tuesday":
+            console.log("Mood On");
+            break;
+
+            case "Wednesday":
+                console.log("work day");
+                break;
+
+                case "Thursday":
+                    console.log("Movie day");
+                    break;
+
+                    case "Friday":
+                        console.log("food day");
+                        break;
+
+                        case "Saturday":
+                            console.log("Weekend");
+                            break;
+
+    default:
+        console.log("Sunday is the holiday");
+        break;
+}
