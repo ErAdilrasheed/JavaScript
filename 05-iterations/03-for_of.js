5e539f21b9016d024ec18ed3f987cf58ed3b08d8
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-02-06 12:08:35 +0000

    for of loops

---
+++ 05-iterations/03-for_of.js
@@ -0,0 +1,15 @@
+// for of loop
+
+//for (const iterator of object) {   here object does mean java script object 
+//}
+
+const numbers = [1,2,3,4,5]
+for (const num of numbers) {
+    //console.log(num);
+}
+
+const knitian = "KNITIAN"
+for (const mca of knitian) {
+    console.log(`Every Char is Loved by MCA ${mca}`);
+    
+}
