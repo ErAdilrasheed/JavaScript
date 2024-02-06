0716c4483c2f438e0a9d048e51b89a6aec05715d
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-02-06 14:15:26 +0000

    for of loops

--- 05-iterations/03-for_of.js
+++ 05-iterations/03-for_of.js
@@ -10,6 +10,27 @@ for (const num of numbers) {
 
 const knitian = "KNITIAN"
 for (const mca of knitian) {
-    console.log(`Every Char is Loved by MCA ${mca}`);
+    //console.log(`Every Char is Loved by MCA ${mca}`);
     
 }
+
+// Map
+
+const map = new Map()
+map.set('IN' , "India")
+map.set('USA' ,"United States of America")
+map.set('FR' , "France")
+map.set('IN' , "India")
+//console.log(map);
+
+for (const [key , value] of map) {
+    //console.log(key , '--->' , value);
+}
+
+const myObject = {
+    'Game1': "FreeFire",
+    'Game2': "BGMI"
+}
+for (const [key , value] of myObject) {
+    console.log(key , '-->' , value);     // Got Error message object is not iterable, 
+}                                         // there is another looping method to iterate object
