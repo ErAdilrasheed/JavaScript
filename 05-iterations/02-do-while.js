48bc1e427e1302c3b2d9cb5704df0d1521527210
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2024-02-06 11:55:21 +0000

    do while loops

---
+++ 05-iterations/02-do-while.js
@@ -0,0 +1,22 @@
+// while 
+let myNum = 2
+while (myNum <= 10) {
+    //console.log("Increment");
+
+    myNum = myNum + 1
+}
+
+let myArray = ["Adil","Aman","Akash","Arham"]
+let arr = 0
+while (arr < myArray.length) {
+  //console.log(` ${myArray[arr]} is my friend`);  
+  arr = arr + 1
+}
+
+// do-while       will execute atleast once
+
+let score = 11
+do {
+  console.log(`Score is ${score}`);
+  score++ 
+} while (score <= 10);
